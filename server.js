ada1544e8ea688627d9b26994ae61a9344a66268
Douglas Gardner <douglas@chippy.ch>
2024-03-30 00:47:56 +0000

    Make a trade

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { NewMessage } = require('telegram/events')
 const instruments = require('./instruments.js')
 const sentiment = require('./sentiment.js')
+const orders = require('./orders.js')
 
 const main = async () => {
   const client = await require('./client.js')()
@@ -8,13 +9,22 @@ const main = async () => {
   client.addEventHandler(async (event) => {
     const msg = event.message
     const sender = await msg.getSender()
+
+    const possibleInstruments = await instruments.searchStringForInstruments(msg.message)
+    const senti = sentiment.getSentiment(msg.message)
     console.log(
       new Date(msg.date * 1000),
       sender.username ? `‹@${sender.username}›` : `«${msg.firstName} ${msg.lastName}»`,
       msg.message,
-      await instruments.searchStringForInstruments(msg.message),
-      sentiment.getSentiment(msg.message)
+      senti
     )
+
+    const quantity = Math.sign(senti)
+    if (quantity === 0) return
+    if (possibleInstruments.length === 0) return
+
+    const order = await orders.placeMarketOrder(possibleInstruments[0].ticker, quantity)
+    console.dir(order)
   }, new NewMessage())
 }
 
